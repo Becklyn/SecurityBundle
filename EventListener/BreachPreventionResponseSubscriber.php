32159a6ef81c2dcb3330b7527d9f25cdbf5b8730
Jannik Zschiesche <jz@becklyn.com>
2017-12-21 15:31:42 +0100

    Log error and fix call

--- EventListener/BreachPreventionResponseSubscriber.php
+++ EventListener/BreachPreventionResponseSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace Becklyn\SecurityBundle\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
@@ -18,6 +19,24 @@ use Symfony\Component\HttpKernel\KernelEvents;
  */
 class BreachPreventionResponseSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct (LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+
+    /**
+     * @param FilterResponseEvent $event
+     */
     public function onResponse (FilterResponseEvent $event)
     {
         if (!$event->isMasterRequest())
@@ -25,28 +44,42 @@ class BreachPreventionResponseSubscriber implements EventSubscriberInterface
             return;
         }
 
-        $response = $event->getResponse();
-        $content = $response->getContent();
+        $this->injectNonce($event->getResponse());
     }
 
 
+    /**
+     * Injects the nonce in the response
+     *
+     * @param Response $response
+     */
     private function injectNonce (Response $response)
     {
-        $content = $response->getContent();
-        $pos = \strripos($content, '</body>');
-
-        if (false === $pos)
+        try
         {
-            return;
+            $content = $response->getContent();
+            $pos = \strripos($content, '</body>');
+
+            if (false === $pos)
+            {
+                return;
+            }
+
+            $randomData = random_bytes(25);
+            $nonce = substr(base64_encode($randomData), 0, random_int(1, 32));
+            $nonceSnippet = "<!-- {$nonce} -->";
+
+            $content = substr($content, 0, $pos) . $nonceSnippet . substr($content, $pos);
+            $response->setContent($content);
         }
-
-        $randomData = random_bytes(25);
-        $nonce = substr(base64_encode($randomData), 0, random_int(1, 32));
-        $nonceSnippet = "<!-- {$nonce} -->";
-
-        $content = substr($content, 0, $pos) . $nonceSnippet . substr($content, $pos);
-        $response->setContent($content);
+        catch (\Exception $e)
+        {
+            $this->logger->error("Could not inject nonce as random data generation failed.", [
+                "innerException" => $e,
+            ]);
+        }
     }
+
 
     /**
      * @inheritdoc
