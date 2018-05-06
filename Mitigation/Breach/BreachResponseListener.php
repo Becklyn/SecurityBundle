584834ad9d7ba9427766f4b2454a54f446f5e742
Jannik Zschiesche <jz@becklyn.com>
2018-05-06 19:21:22 +0200

    Don’t modify unsupported responses

--- Mitigation/Breach/BreachResponseListener.php
+++ Mitigation/Breach/BreachResponseListener.php
@@ -4,6 +4,10 @@ namespace Becklyn\SecurityBundle\Mitigation\Breach;
 
 use Becklyn\SecurityBundle\Html\HtmlNonceInjector;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -44,9 +48,22 @@ class BreachResponseListener implements EventSubscriberInterface
         }
 
         $response = $event->getResponse();
-        $response->setContent(
-            $this->nonceInjector->injectNonce($response->getContent())
-        );
+
+        switch (true)
+        {
+            // don't modify unsupported responses
+            case $response instanceof BinaryFileResponse:
+            case $response instanceof JsonResponse:
+            case $response instanceof StreamedResponse:
+            case $response instanceof RedirectResponse:
+                return;
+
+            default:
+                $response->setContent(
+                    $this->nonceInjector->injectNonce($response->getContent())
+                );
+                break;
+        }
     }
 
 
